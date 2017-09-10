431fd61b02d1a4207c35ef98f1a2738b1aaf6434
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-09 19:51:06 -0500

    added circles to scale

--- app/scale.js
+++ app/scale.js
@@ -2,8 +2,16 @@
 
 //-------------------SCALE -------------------------//
 let width = 1000;
+let height = 100;
 //height of world's tallest buildings
-let data = [2717, 2073, 1971, 1776, 1670];
+// let data = [2717, 2073, 1971, 1776, 1670];
+let data = [
+	[ "Burj Khalifa" , 2717],
+	[ "Shanghai Tower" , 2073],
+	[ "Makkah Royal Clock Tower Hotel", 1971],
+	[ "One World Trade Center" , 1776],
+	[ "Taipei 101" , 1670]
+];
 
 //create SVG appended to div.main
 let mySVG = d3.select('.main')
@@ -12,7 +20,7 @@ let mySVG = d3.select('.main')
 
 //map length of SVG to max value of data set
 let scale = d3.scale.linear()
-				.domain([0, d3.max(data)])
+				.domain([0, d3.max(data, d => {return d[1] + 100;})])
 				.range([10, width]);
 
 //create an axis with this scale
@@ -20,4 +28,17 @@ let axis = d3.svg.axis()
 				.scale(scale);
 
 //add axis to svg
-mySVG.call(axis);
+mySVG.append('g').call(axis).attr('class', 'axis-style')
+				.attr('transform', 'translate(0,' + height + ')');
+
+
+//-------------------Add Circles to Scale for Each Data Point -------------------------//
+mySVG.selectAll('circle').data(data).enter()
+						.append('circle')
+						.attr('r', 4)
+						.attr('transform', d => {return 'translate(' + scale(d[1]) + ',' + height + ')';});
+
+//-------------------Add Labels for Each Data Point -------------------------//
+mySVG.selectAll('text').data(data).enter()
+						.append('text').text(d => {return d[0];})
+						.attr('transform', function(d) {return 'translate(' + scale(d[1]) + ',' + height + ')' + 'rotate(-20) translate(5, -5)';});
